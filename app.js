52702ca29bd7c208033a3a82c27ef9648ac2b7a4
ZL1996 <649349296@qq.com>
2016-08-08 21:40:20 +0800

    first

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-const express = require('express');
-const app = express();
 
+'use strict';
+
+let express = require('express');
+let app = express();
 app.use(express.static('public'));
 
-app.get('/message', (req, res) => {
-	res.status(200).send('This is a success message');
+const BarcodeToCode = require('./public/core/barcodeToCode');
+const CodeToBarcode = require('./public/core/codeToBarcode');
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+app.post('/barcodeToCode',function (req,res) {
+	let input = req.body.barcode;
+	console.log(input);
+	let result = new BarcodeToCode().changeBarcodeToCode(input);
+	// console.log(result);
+	res.send(result);
 });
 
-app.listen(3000);
+app.post('/codeToBarcode',function (req,res){
+	let input = req.body.code;
+	console.log(input);
+	let result = new CodeToBarcode().changCodeToBarcode(input);
+	res.send(result);
+});
+
+app.listen(3000,function () {
+	console.log("server 3000 is started");
+});
+
+module.exports = app;
--- public/scripts/my-script.js
+++ public/scripts/my-script.js
@@ -1,9 +1,38 @@
-$(function () {
-	$.get('/message')
-		.done(function (data) {
-			$('#title').text(data);
-		})
-		.fail(function (error) {
-			$('#title').text('O_o');
-		});
+
+$(document).ready(function(){
+    $('#input_code').focus(function () {
+        $('#err_msg').hide();
+        $('#result').hide();
+    });
+    $('#ok').click(function(){
+        var input = $('#input_code').val();
+        if($('#barToCode').is(":checked")){
+            if(isBarcodeLegal(input)){
+                $.post('barcodeToCode',{barcode:$('#input_code').val()})
+                    .done(function (data) {
+                        $('#result').text(data);
+                        $('#result').show();
+                        $('#history').after('<tr><td>'+input+'</td><td>'+data+'</td></tr>')
+                    })
+            } else {
+                $('#err_msg').show();
+            }
+        }
+        if($('#codeToBarcode').is(':checked')){
+            if(isCodeLegal(input)){
+                $.post('/codeToBarcode',{
+                    code:$('#input_code').val()
+                },function(data,status){
+                    $('#result').text(data);
+                    $('#result').show();
+                    $('#history').after('<tr><td>'+input+'</td><td>'+data+'</td></tr>')
+                })
+            } else {
+                $('#err_msg').show();
+            }
+        }
+    });
+});
+$('#inputs_code').focusin(function(){
+    $('#err_msg').hide();
 });
